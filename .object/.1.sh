c30d0a3bf92b2ae40d3beba99f9c580efc88c116
Descodia <164511612+Descodia@users.noreply.github.com>
2024-03-23 21:07:26 +0200

    Update .1.sh

--- .object/.1.sh
+++ .object/.1.sh
@@ -3,7 +3,7 @@ PUT(){ echo -en "\033[${1};${2}H";}
 clear;toilet -t -f mono12  "Descodia" --gay -F border 
 echo ""
 PUT 11 40
-echo -e "\e[92mBy\e[1;93m RajAryan\e[1;95m /\e[1;96m H4Ck3R0 "
+echo -e "\e[92mBy\e[1;93m RajAryan\e[1;95m /\e[1;96m Ahmed "
 PUT 12 3
 echo
 echo -e "\e[1;31m  [\e[32m√\e[31m] \e[1;91m by \e[1;36mRaj Aryan \e[93m/ \e[100;92m_raj\e[0m"
